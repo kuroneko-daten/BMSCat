7fc340f34e4b1bedfb463d1489304c1ced2619d2
96neko_daten <96neko.daten@gmail.com>
2017-09-07 02:18:56 +0900

    add setter/getter

--- bms_model.h
+++ bms_model.h
@@ -6,6 +6,34 @@ public:
     BmsModel();
     ~BmsModel();
 
+    //setter
+    void set_path(std::string);
+    void set_genre(std::string);
+    void set_title(std::string);
+    void set_subtitle(std::string);
+    void set_artist(std::string);
+    void set_subartist(std::string);
+    void set_bpm(double);
+    void set_total(double);
+    void set_level(int);
+    void set_notes(int);
+    void set_player(int);
+    void set_rank(int);
+
+    //getter
+    std::string get_path();
+    std::string get_genre();
+    std::string get_title();
+    std::string get_subtitle();
+    std::string get_artist();
+    std::string get_subartist();
+    double get_bpm();
+    double get_total();
+    int get_level();
+    int get_notes();
+    int get_player();
+    int get_rank();
+
 private:
     std::string path_ = "";         //bms file path
     std::string genre_ "";          //genre
