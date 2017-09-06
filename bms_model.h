877295b0f2a5321753b6ebe75d499b69fb7c7920
96neko_daten <96neko.daten@gmail.com>
2017-09-07 01:57:37 +0900

    add member variables

--- bms_model.h
+++ bms_model.h
@@ -6,4 +6,17 @@ public:
     BmsModel();
     ~BmsModel();
 
+private:
+    std::string path_ = "";         //bms file path
+    std::string genre_ "";          //genre
+    std::string title_ "";          //title
+    std::string subtitle =  "";     //subtitle
+    std::string artist_ = "";       //artist
+    std::string subartist_ = "";    //subartist
+    double bpm_ = 130;              //bpm (default 130)
+    double total_ = 0;              //total (default )
+    int level_ = 0;                 //level (default 0)
+    int notes_ = 0;                 //total notes (exclude invisible)
+    int player_ = 0;                //play style
+    int rank_ = 0;                  //judge rank
 };
